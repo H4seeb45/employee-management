0afcd7fb23704ea6236049b075231ce394997222
H4seeb45 <ahmed.haseeb56@gmail.com>
2026-02-19 12:54:54 +0500

    [fix] Renamed old and added new expense types

--- components/expenses/expense-types.ts
+++ components/expenses/expense-types.ts
@@ -1,52 +1,161 @@
 export const expenseTypes = [
-  { value: "CASH_LOADERS", label: "Cash Loaders" },
+  { value: "CASH_LOADERS", label: "Daily Wages" },
   { value: "CLAIMABLES_PAYMENT", label: "Claimables - Payment" },
-  { value: "CLEANING_ITEMS", label: "Cleaning Items" },
+  { value: "CLEANING_ITEMS", label: "Cleaning Material" },
   { value: "CONSULTANCY_CHARGES", label: "Consultancy Charges" },
   { value: "CONTAINER_UNLOADING_CHARGES", label: "Container Unloading Charges" },
-  { value: "COOLER_RENTAL", label: "Cooler Rental" },
-  { value: "COURIER_CHARGES", label: "Courier Charges" },
+  { value: "COOLER_RENTAL", label: "Coolers Expenses" },
+  { value: "COURIER_CHARGES", label: "Courier Expenses" },
   { value: "DAILY_KITCHEN_EXP", label: "Daily Kitchen Exp." },
-  { value: "DMS_PAYMENT", label: "DMS - Payment" },
-  { value: "ELECTRICITY_BILL", label: "Electricity Bill" },
-  { value: "EMPLOYEE_ACCOMODATION_EXP", label: "Employee Accomodation Exp." },
-  { value: "EMPLOYEE_MOBILE_EXP", label: "Employee Mobile Exp." },
-  { value: "EMPLOYEE_TRAVELLING_EXP", label: "Employee Travelling Exp." },
-  { value: "EMPTY_SORTING", label: "Empty Sorting" },
-  { value: "ENTERTAINMENT", label: "Entertainment" },
-  { value: "EOBI", label: "EOBI" },
-  { value: "FUEL_FOR_BIKE", label: "Fuel For Bike" },
-  { value: "GAS_BILL", label: "Gas Bill" },
-  { value: "INTERNET_BILL", label: "Internet Bill" },
-  { value: "LEGAL_CHARGES", label: "Legal and Government Charges" },
-  { value: "LOADERS_ACCOUMODATION_EXP", label: "Loaders Accoumodation Exp." },
-  { value: "LOADING_CHARGES", label: "Loading Charges" },
+  { value: "DMS_PAYMENT", label: "Computer Exp" },
+  { value: "ELECTRICITY_BILL", label: "Electricity Expenses" },
+  { value: "EMPLOYEE_ACCOMODATION_EXP", label: "House Rent" },
+  { value: "EMPLOYEE_MOBILE_EXP", label: "Telephone Exp" },
+  { value: "EMPLOYEE_TRAVELLING_EXP", label: "Travelling & Convayance" },
+  { value: "EMPTY_SORTING", label: "Empty Sorting Expenses" },
+  { value: "ENTERTAINMENT", label: "Entertainment Exp" },
+  { value: "EOBI", label: "EOBI Payment" },
+  { value: "FUEL_FOR_BIKE", label: "Bike Fuel" },
+  { value: "GAS_BILL", label: "Gas Expenses" },
+  { value: "INTERNET_BILL", label: "Internet Exp ( Email/internet Bill )" },
+  { value: "LEGAL_CHARGES", label: "Legal & Professional Charges" },
+  { value: "LOADERS_ACCOUMODATION_EXP", label: "Loaders House Rent" },
+  { value: "LOADING_CHARGES", label: "Loading Expense" },
   { value: "MEETING_EXP", label: "Meeting Exp." },
   { value: "PACKING_MATERIAL", label: "Packing Material" },
-  { value: "PRINTER_SERVICE", label: "Printer Service" },
-  { value: "PRINTING_STATIONERY", label: "Printing & Stationery" },
-  { value: "REPAIR_MAINT_GENERAL", label: "Repair & Maint. General" },
-  { value: "REPAIR_MAINT_VEHICLE", label: "Repair & Maint. Vehicle" },
-  { value: "REPAIR_MAINT_WAREHOUSE", label: "Repair & Maint. Warehouse" },
-  { value: "RIKSHAW_RENTAL", label: "Rikshaw Rental" },
+  { value: "PRINTER_SERVICE", label: "Printer Services" },
+  { value: "PRINTING_STATIONERY", label: "Printing & Stationary" },
+  { value: "REPAIR_MAINT_GENERAL", label: "Repair & Maintinence Exp" },
+  { value: "REPAIR_MAINT_VEHICLE", label: "Vehicle Repir & Maintenance" },
+  { value: "REPAIR_MAINT_WAREHOUSE", label: "Repair & Maintenence ( Billa Denter)" },
+  { value: "RIKSHAW_RENTAL", label: "Rikshaw Rental Expenses" },
   { value: "SECP_FEE", label: "SECP Fee" },
-  { value: "SECURITY_CAMERA_SERVICE", label: "Security Camera Service" },
+  { value: "SECURITY_CAMERA_SERVICE", label: "Security Camera Servives" },
   { value: "TAX_PAYMENT", label: "Tax Payment" },
-  { value: "TOLLS_TAXES", label: "Tolls & Taxes" },
-  { value: "TRAFFIC_POLICE", label: "Traffic Police" },
-  { value: "VEHICLE_CHALLAN", label: "Vehicle Challan" },
-  { value: "VEHICLE_FUEL", label: "Vehicle Fuel" },
-  { value: "VEHICLE_RENTAL", label: "Vehicle Rental" },
-  { value: "VEHICLES_RENT", label: "Vehicles Rent" },
-  { value: "WAREHOUSE_RENT", label: "Warehouse Rent" },
-  { value: "WORKER_WELFARE", label: "Worker Welfare" },
-  { value: "INVESTOR_PAYMENT", label: "Investor Payment" },
-  { value: "VEHICLE_PARKING", label: "Vehicle Parking" },
-  { value: "VEHICLE_PASSING", label: "Vehicle Passing" },
-  { value: "GUARD_TEA_EXP", label: "Guard Tea Exp." },
-  { value: "GARBAGE_COLLECTION", label: "Garbage Collection" },
+  { value: "TOLLS_TAXES", label: "Toll Tax Expense" },
+  { value: "TRAFFIC_POLICE", label: "Monthly Traffic Police Payment" },
+  { value: "VEHICLE_CHALLAN", label: "Trafic Challans (Police)" },
+  { value: "VEHICLE_FUEL", label: "Vehicle Fuel Expenses" },
+  // { value: "VEHICLE_RENTAL", label: "Vehicle Rental" },
+  { value: "VEHICLES_RENT", label: "Van Rent Expenses" },
+  { value: "WAREHOUSE_RENT", label: "Rent Expense Warehouse" },
+  { value: "WORKER_WELFARE", label: "Staff Wellfare Expenses" },
+  { value: "INVESTOR_PAYMENT", label: "Investor Payments" },
+  { value: "VEHICLE_PARKING", label: "Vehicle Parking Expenses" },
+  { value: "VEHICLE_PASSING", label: "Vehicle Passing Expenses" },
+  { value: "GUARD_TEA_EXP", label: "Monthly Guards Tea Voucher" },
+  { value: "GARBAGE_COLLECTION", label: "Monthly Garbage Expenses" },
   { value: "FIXED_ASSET", label: "Fixed Asset" },
-  { value: "INCENTIVE_PAYMENT", label: "Incentive Payment" },
-  { value: "RAMZAN_IFTAR", label: "Ramzan Iftar" },
-  { value: "WATER_BILL", label: "Water Bill" },
+  { value: "INCENTIVE_PAYMENT", label: "Salary & Incentives" },
+  { value: "RAMZAN_IFTAR", label: "Ramazan Iftar" },
+  { value: "WATER_BILL", label: "Water Expense" },
+  // NEW
+  {
+    label: "Rgb Breakage (Warehouse/Plant)",
+    value: "RGB_BREAKAGE_WAREHOUSE_PLANT"
+  },
+  {
+    label: "Audit Expense",
+    value: "AUDIT_EXPENSE"
+  },
+  {
+    label: "Bank Charges",
+    value: "BANK_CHARGES"
+  },
+  {
+    label: "Bad Debts Expenses",
+    value: "BAD_DEBTS_EXPENSES"
+  },
+  {
+    label: "Bike Repair",
+    value: "BIKE_REPAIR"
+  },
+  {
+    label: "Charity & Donation",
+    value: "CHARITY_DONATION"
+  },
+  {
+    label: "Eid Bonus Expense",
+    value: "EID_BONUS_EXPENSE"
+  },
+  {
+    label: "Empty/Glass Sale Discount",
+    value: "EMPTY_GLASS_SALE_DISCOUNT"
+  },
+  {
+    label: "Extra Discount Expenses",
+    value: "EXTRA_DISCOUNT_EXPENSES"
+  },
+  {
+    label: "Extra Supply Expenses",
+    value: "EXTRA_SUPPLY_EXPENSES"
+  },
+  {
+    label: "Fee And Subscriptions",
+    value: "FEE_AND_SUBSCRIPTIONS"
+  },
+  {
+    label: "Fine & Penalties",
+    value: "FINE_PENALTIES"
+  },
+  {
+    label: "FOC Stock (Office Use)",
+    value: "FOC_STOCK_OFFICE_USE"
+  },
+  {
+    label: "Group Life & Health Insurance Expense",
+    value: "GROUP_LIFE_HEALTH_INSURANCE_EXPENSE"
+  },
+  {
+    label: "Head Office Expense Account",
+    value: "HEAD_OFFICE_EXPENSE_ACCOUNT"
+  },
+  {
+    label: "Insurance Exp Leased Vehicles",
+    value: "INSURANCE_EXP_LEASED_VEHICLES"
+  },
+  {
+    label: "Loss Or Gain On Assets & Reversal Of Price On Stocks",
+    value: "LOSS_GAIN_ASSETS_REVERSAL_PRICE_STOCKS"
+  },
+  {
+    label: "Low Fill/Brust/Ptn Short",
+    value: "LOW_FILL_BRUST_PTN_SHORT"
+  },
+  {
+    label: "Management Expenses",
+    value: "MANAGEMENT_EXPENSES"
+  },
+  {
+    label: "Mark Up On Leased Vehicles",
+    value: "MARK_UP_ON_LEASED_VEHICLES"
+  },
+  {
+    label: "Medical Expense",
+    value: "MEDICAL_EXPENSE"
+  },
+  {
+    label: "Salary And Wages",
+    value: "SALARY_AND_WAGES"
+  },
+  {
+    label: "Social Security Expenses",
+    value: "SOCIAL_SECURITY_EXPENSES"
+  },
+  {
+    label: "Staff Annual Leave Encashment Expenses",
+    value: "STAFF_ANNUAL_LEAVE_ENCASHMENT_EXPENSES"
+  },
+  {
+    label: "Transportation Charges",
+    value: "TRANSPORTATION_CHARGES"
+  },
+  {
+    label: "Uniform Expenses (PSO & DM)",
+    value: "UNIFORM_EXPENSES_PSO_DM"
+  },
+  {
+    label: "Zakat Expenses",
+    value: "ZAKAT_EXPENSES"
+  }
 ];
